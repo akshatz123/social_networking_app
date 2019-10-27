1e1bac4c9cf0abcc47a8cf88c5be6d03b949b81b
Akshat Zala <akshatzala@gmail.com>
2019-10-27 10:58:09 +0530

    Added gitignore file

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class Posts(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(blank=True)
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -25,6 +25,7 @@ class PostListView(ListView):
 	ordering = ['-date_posted']
 	paginate_by = 5
 
+
 class UserPostListView(ListView):
 	model = Posts
 	template_name = 'blog/user_posts.html'
@@ -34,6 +35,7 @@ class UserPostListView(ListView):
 	def get_queryset(self):
 		user = get_object_or_404(User, username=self.kwargs.get('username'))
 		return Posts.objects.filter(author=user).order_by('-date_posted')
+
 
 class PostDetailView(DetailView):
 	model = Posts
