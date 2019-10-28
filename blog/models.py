70111736c70a096b3674f4dd16fb64608a27efd3
Akshat Zala <akshatzala@gmail.com>
2019-10-28 11:40:01 +0530

    Removed files

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from PIL import Image
 
 
 class Posts(models.Model):
@@ -9,9 +10,20 @@ class Posts(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='media/posts/', null=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url (self):
-    	return reverse('post-detail', kwargs={'pk': self.pk})
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- blog/views.py
+++ blog/views.py
@@ -2,79 +2,80 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.views.generic import (
-	ListView, 
-	DetailView,
-	CreateView,
-	UpdateView,
-	DeleteView
-) 
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Posts
 
 
 def home(request):
-	context={
-		'posts':  Posts.objects.all(),		
-	}
-	return render(request, 'blog/home.html', context)
+    context = {
+        'posts': Posts.objects.all(),
+    }
+    return render(request, 'blog/home.html', context)
 
 
 class PostListView(ListView):
-	model = Posts
-	template_name = 'blog/home.html'
-	context_object_name = 'posts'
-	ordering = ['-date_posted']
-	paginate_by = 5
+    model = Posts
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
 
 
 class UserPostListView(ListView):
-	model = Posts
-	template_name = 'blog/user_posts.html'
-	context_object_name = 'posts'
-	paginate_by = 5
+    model = Posts
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
 
-	def get_queryset(self):
-		user = get_object_or_404(User, username=self.kwargs.get('username'))
-		return Posts.objects.filter(author=user).order_by('-date_posted')
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Posts.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
-	model = Posts
+    model = Posts
+    fields = ['title', 'content', 'image']
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
-	model = Posts
-	fields = ['title', 'content']
+    model = Posts
+    fields = ['title', 'content', 'image']
 
-	def form_valid(self, form):
-		form.instance.author = self.request.user
-		return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-	model = Posts
-	fields = ['title', 'content']
+    model = Posts
+    fields = ['title', 'content', 'image']
 
-	def form_valid(self, form):
-		form.instance.author = self.request.user
-		return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
-	def test_func(self):
-		post = self.get_object()
-		if self.request.user == post.author:
-			return True
-		return False
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-	model = Posts
-	success_url = '/'
-	def test_func(self):
-		post = self.get_object()
-		if self.request.user == post.author:
-			return True
-		return False
+    model = Posts
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
-	return render(request, 'blog/about.html', {'title':'About',})
-
-
+    return render(request, 'blog/about.html', {'title': 'About', })
