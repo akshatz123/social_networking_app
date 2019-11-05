155c6787a98911227588db4e5bd6d66275ddceae
Akshat Zala <akshat.zala@cuelogic.com>
2019-11-05 21:42:01 +0530

    Email Verification

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^friend-request/send/(?P<id>[\w-]+)/$', send_friend_request, name='send_friend_request'),
     url(r'^friend-request/cancel/(?P<id>[\w-]+)/$', cancel_friend_request, name='cancel_friend_request'),
     url(r'^friend-request/accept/(?P<id>[\w-]+)/$', accept_friend_request, name='accept_friend_request'),
+    url(r'^friend-request/accept/(?P<id>[\w-]+)/$', accept_friend_request, name='accept_friend_request'),
     path('search/', search, name='friends_search'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         activate_account, name='activate'),
