5151b8facb4fdcfa580fe6147e05e0a2db73f533
Akshat Zala <akshat.zala@cuelogic.com>
2019-11-11 12:07:06 +0530

    Searched for particular author's blog

--- django_project/settings.py
+++ django_project/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sample_blog',
         'USER': 'root',
-        'PASSWORD': 'password',
+        'PASSWORD': '1234root',
         'HOST': 'localhost',
         'PORT': '3306'
     }
